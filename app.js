2ff66fd64fbd4bb1f400fc64fe56fe3b92080aad
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-11-16 22:39:04 +0300

    filtering using post

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express') ;
+const bodyParser = require('body-parser') ;
 
 const app = express() ;
 
+app.use(bodyParser.urlencoded({extended : false})) ;
+
+// Route handling middlewares
 app.use('/add-product', (req, res, next) =>{      
     res.send('<form action="/product" method="POST"><input type="test" name = "title"><button>Submit</button><form>') ;
 }) ;
 
-app.use('/product', (req, res, next) =>{      
+// app.use('/product', (req, res, next) =>{   
+//     console.log(req.body) ;
+//     res.redirect('/') ;
+// }) ;                             //Handling all HTTP methods. 
+
+app.post('/product', (req, res, next) =>{   
+    console.log(req.body) ;
     res.redirect('/') ;
 }) ;
 
