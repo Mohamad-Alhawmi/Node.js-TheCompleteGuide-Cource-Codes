3eca8cb08ab4a3dc89c99b7a26993d5d524aa627
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-11-17 20:33:22 +0300

    Added 404 page

--- Online Shop Application/routes/admin.js
+++ Online Shop Application/routes/admin.js
@@ -5,7 +5,7 @@ const express = require('express') ;
 const router = express.Router() ;
 
 // /admin/add-product => GET
-router.get('/add-product', (req, res, next) =>{      
+router.get('/add-product', (req, res, next) =>{   
     res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html')) ;
 }) ;
 
