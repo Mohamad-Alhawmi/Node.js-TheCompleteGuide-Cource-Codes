6286324de2ae411c92c1de9835d547065a670944
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-11-17 12:38:46 +0300

    separated routes into different files

--- app.js
+++ app.js
@@ -1,27 +1,14 @@
 const express = require('express') ;
 const bodyParser = require('body-parser') ;
 
+const adminRoutes = require('./routes/admin') ;
+const shopRoutes = require('./routes/shop') ;
+
 const app = express() ;
 
 app.use(bodyParser.urlencoded({extended : false})) ;
 
-// Route handling middlewares
-app.use('/add-product', (req, res, next) =>{      
-    res.send('<form action="/product" method="POST"><input type="test" name = "title"><button>Submit</button><form>') ;
-}) ;
-
-// app.use('/product', (req, res, next) =>{   
-//     console.log(req.body) ;
-//     res.redirect('/') ;
-// }) ;                             //Handling all HTTP methods. 
-
-app.post('/product', (req, res, next) =>{   
-    console.log(req.body) ;
-    res.redirect('/') ;
-}) ;
-
-app.use('/', (req, res) =>{  
-    res.send('<h1>Hello from Express!</h1>') ;
-}) ; 
+app.use(adminRoutes) ;
+app.use(shopRoutes) ;
 
 app.listen(3000) ; 
