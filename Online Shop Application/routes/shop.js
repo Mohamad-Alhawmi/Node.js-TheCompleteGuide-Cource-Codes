4a771a63a9ebd1bf218ff2c6b871460c122e39ff
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-11-18 12:23:53 +0300

    Fixed Errors

---
+++ Online Shop Application/routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path') ;
+
+const express = require('express') ;
+
+const rootDirectory = require('../util/path') ; 
+
+const router = express.Router() ;
+
+router.get('/', (req, res) =>{  
+    res.sendFile(path.join(rootDirectory, 'views', 'shop.html')) ;
+}) ; 
+
+module.exports = router ;
+
