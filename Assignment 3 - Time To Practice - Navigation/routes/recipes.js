4142b6c94f734fc40ad3bb6d5b321f8f8c1454ba
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-11-19 04:22:17 +0300

    Added missing semicolon

--- Assignment 3 - Time To Practice - Navigation/routes/recipes.js
+++ Assignment 3 - Time To Practice - Navigation/routes/recipes.js
@@ -7,7 +7,7 @@ const rootDirectory = require('../util/path') ;
 const router = express.Router() ;
 
 router.get('/recipes', (res, req, next) =>{
-    sendFile(path.join(rootDirectory, 'views', 'recipes.html')) ;
-})
+    sendFile(path.join(rootDirectory, 'views', 'recipes.html'))
+}) ;
 
 module.exports = router ;
