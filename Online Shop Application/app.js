961566f23cbcbf1d96296334ce580816ccc6c35a
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-12-06 15:40:56 +0300

    Added controllers files

--- Online Shop Application/app.js
+++ Online Shop Application/app.js
@@ -3,7 +3,9 @@ const path = require('path') ;
 const express = require('express') ;
 const bodyParser = require('body-parser') ;
 
-const adminData = require('./routes/admin') ;
+const errorController = require('./contollers/error') ; ;
+
+const adminRoutes = require('./routes/admin') ;
 const shopRoutes = require('./routes/shop') ;
 
 const app = express() ;
@@ -15,12 +17,10 @@ app.set('views', 'views') ;
 app.use(bodyParser.urlencoded({extended : false})) ;
 app.use(express.static(path.join(__dirname, 'public'))) ;
 
-app.use('/admin', adminData.routes) ;
+app.use('/admin', adminRoutes) ;
 app.use(shopRoutes) ;
 
-app.use((req, res, next) =>{
-    res.status(404).render('404', {pageTitle : 'Page Not Found', path : ''}) ; // Returned empty path to fix the error.
-})
+app.use(errorController.get404) ;
 
 app.listen(3000) ; 
 
