86aa01bf272ea8437a4258376da55d3d8c067a53
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2019-12-28 23:42:04 +0300

    Added a cart model

--- Online Shop Application/contollers/shop.js
+++ Online Shop Application/contollers/shop.js
@@ -1,22 +1,20 @@
 const Product = require('../models/product') ;
+const Cart = require('../models/cart') ;
 
-exports.getProducts = (req, res) =>
+exports.getProducts = (req, res, next) =>
 {  
-    Product.fetchAll((products) =>
+    Product.fetchAll( products =>
     {
         res.render('shop/product-list', 
         {
             prods : products,
             pageTitle : 'All Products',
-            path : '/products',
-            hasProducts : products.length > 0,
-            activeShop : true,
-            productCSS : true
+            path : '/products'
         }) ;
     }) ;
 } ;
 
-exports.getProduct = (req, res) =>
+exports.getProduct = (req, res, next) =>
 {  
     const prodId = req.params.productId ;
     Product.findById(prodId, product =>{
@@ -45,9 +43,18 @@ exports.getCart = (req, res, next) =>
 {
     res.render('shop/cart', 
     {
-        pageTitle : 'Your Cart',
-        path : '/cart'
+        path : '/cart',
+        pageTitle : 'Your Cart'
     }) ;
+} ;
+
+exports.postCart = (req, res, next) =>
+{
+    const prodId = req.body.productId ;
+    Product.findById(prodId, product => {
+        Cart.addProduct(prodId, product.price) ;
+    }) ;
+    res.redirect('/cart') ;
 } ;
 
 exports.getOrders = (req, res, next) =>
