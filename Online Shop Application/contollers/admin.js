86bec1d303402cf4d631855cb0eff9191641857a
Mohamad Alhamwi <moda.tnt.edu@outlook.com>
2020-01-24 18:45:04 +0300

    Fixed edit-product functionaliy

--- Online Shop Application/contollers/admin.js
+++ Online Shop Application/contollers/admin.js
@@ -17,23 +17,23 @@ exports.postAddProduct = (req, res, next) =>
     const price = req.body.price;
     const description = req.body.description;
     
-    Product.create(
-    {
+    req.user.createProduct
+    ({
       title : title,
       price : price,
       imageUrl : imageUrl,
       description : description
     })
-        .then(result => 
-        {
-            
-            console.log('Created Product');
-            res.redirect('/admin/products');
-        })
-        .catch(err => 
-        {
-            console.log(err);
-        });
+    .then(result => 
+    {
+        
+        console.log('Created Product');
+        res.redirect('/admin/products');
+    })
+    .catch(err => 
+    {
+        console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) =>
@@ -47,26 +47,34 @@ exports.getEditProduct = (req, res, next) =>
 
     const prodId = req.params.productId ;
 
-    Product.findByPk(prodId)
-        .then((product) =>
+    req.user.getProducts
+    (
         {
-            if(!editMode)
-            {
-                return res.redirect('/') ;
-            }
+            where : {id : prodId}
+        }
+    )
+    .then((products) =>
+    {
+        const product = products[0] ;
+        console.log(products) ;
+        console.log(product) ;
+        if(!product)
+        {
+            return res.redirect('/') ;
+        }
 
-            res.render('admin/edit-product',
-            {
-                pageTitle : 'Edit Product',
-                path : 'admin/edit-product',
-                editing : editMode,
-                product : product
-            }) ;
-        })
-        .catch((err) =>
+        res.render('admin/edit-product',
         {
-            console.log(err) ;
-        }) ; 
+            pageTitle : 'Edit Product',
+            path : 'admin/edit-product',
+            editing : editMode,
+            product : product
+        }) ;
+    })
+    .catch((err) =>
+    {
+        console.log(err) ;
+    }) ; 
 } ;
 
 exports.postEditProduct = (req, res, next) =>{
@@ -99,7 +107,7 @@ exports.postEditProduct = (req, res, next) =>{
 
 exports.getProducts = (req, res, next) =>
 {
-    Product.findAll()
+    req.user.getProducts()
     .then((products) =>
     {
         res.render('admin/products', 
